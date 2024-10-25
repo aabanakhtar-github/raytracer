0b66ab0336510ff7c33e9ae147709961b5193724
aabanakhtar-github <aabanakhtar.home@gmail.com>
2024-10-24 22:38:48 -0400

    teehee

--- src/Color.h
+++ src/Color.h
@@ -2,9 +2,17 @@
 #define COLOR_H
 
 #include <cstdint>
+#include <ostream>
 
 struct Color {
-  double R, G, B, A;
+  double R = 0.0;
+  double G = 0.0;
+  double B = 0.0;
 };
+
+inline auto operator<<(std::ostream &out,
+                       const Color &color) -> std::ostream & {
+  return out << color.R * 255 << " " << color.G * 255 << " " << color.B * 255;
+}
 
 #endif // !COLOR_H
